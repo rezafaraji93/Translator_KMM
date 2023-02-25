c6f33c475bb344b1512e2c7be0b3113616bcfc0c
rezafaraji <faraji.reza@outlook.com>
2023-02-25 12:24:41 +0330

    implemented Ios Side

---
+++ iosApp/iosApp/translate/presentation/components/SwapLanguageButton.swift
@@ -0,0 +1,29 @@
+//
+//  SwapLanguageButton.swift
+//  iosApp
+//
+//  Created by Abron Studio on 25/02/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwapLanguageButton: View {
+    var onClick: () -> Void
+    var body: some View {
+        Button(action: onClick) {
+            Image(uiImage: UIImage(named: "swap_languages")!)
+                .padding()
+                .background(Color.primaryColor)
+                .clipShape(Circle())
+        }
+    }
+}
+
+struct SwapLanguageButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SwapLanguageButton(
+            onClick: {}
+        )
+    }
+}
